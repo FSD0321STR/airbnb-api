d0e9d635605a45d0527f42de6d1e0741983bbc72
Montserrat Cifuentes <cotamon88@gmail.com>
2021-07-07 21:16:35 +0200

    "borrando schemas no utiles"

--- src/models/mongoose.js
+++ src/models/mongoose.js
@@ -9,17 +9,6 @@ const uri = process.env.MONGODB_URI;
 
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-const Task = mongoose.model('Task', {
-    title: String,
-    completed: { type: Boolean, default: false },
-    board: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Board',
-    },
-    createdAt: { type: Date, default: Date.now },
-    updatedAt: { type: Date, default: Date.now },
-});
-
 const Tipos = mongoose.model('Tipos', {
     title: String,
     visible: Boolean,
@@ -34,19 +23,6 @@ const Servicios = mongoose.model('Servicios', {
     updatedAt: { type: Date, default: Date.now },
 });
 
-const Board = mongoose.model('Board', {
-    title: String,
-    tasks: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Task'
-    }],
-    user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-    },
-    createdAt: { type: Date, default: Date.now },
-    updatedAt: { type: Date, default: Date.now },
-});
 
 const userSchema = new mongoose.Schema({
     name: String,
@@ -81,70 +57,12 @@ userSchema.pre('save', async function(next) {
 
 const User = mongoose.model('User', userSchema);
 
-const taskCreateSchema = {
-    type: "object",
-    properties: {
-        title: { type: "string" },
-        board: { type: "string" },
-    },
-    required: ["title", "board"],
-    additionalProperties: false
-};
 
 
-const taskUpdateSchema = {
-    type: "object",
-    properties: {
-        title: { type: "string" },
-        completed: { type: "boolean" },
-        board: { type: "string" },
-    },
-    required: ["title", "completed"],
-    additionalProperties: false
-};
 
-
-const taskPatchSchema = {
-    type: "object",
-    properties: {
-        title: { type: "string" },
-        completed: { type: "boolean" },
-    },
-    required: [],
-    additionalProperties: false
-};
-
-const boardSchema = {
-    type: "object",
-    properties: {
-        title: { type: "string" }
-    },
-    required: ["title"],
-    additionalProperties: false
-};
-
-
-validateTask = (document, method) => {
-    switch (method) {
-        case 'POST':
-            return ajv.validate(taskCreateSchema, document);
-        case 'PUT':
-            return ajv.validate(taskUpdateSchema, document);;
-        case 'PATCH':
-            return ajv.validate(taskPatchSchema, document);;
-    }
-}
-
-validateBoard = (document) => {
-    return ajv.validate(boardSchema, document);
-}
 
 module.exports = {
-    Task,
-    Board,
     User,
     Tipos,
     Servicios,
-    validateTask,
-    validateBoard,
 }
